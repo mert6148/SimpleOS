2b1872c9ccda0b36c1924dbdcc45e41c0ba47f1c
mert6148 <154323978+mert6148@users.noreply.github.com>
2026-04-24 14:04:25 +0300

    Add files via upload

---
+++ google documantation/script.js
@@ -0,0 +1,7 @@
+const googleDoc = document.getElementById('google-doc');
+
+// ============================================================
+// API Tabs Functionality
+// ============================================================
+const apiTabButtons = document.querySelectorAll('.api-tab-button');
+const apiTabs = document.querySelectorAll('.api-tab');
