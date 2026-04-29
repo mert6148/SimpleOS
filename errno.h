75a179cf8480f34f463498645f41474f8da6cae4
mert6148 <154323978+mert6148@users.noreply.github.com>
2026-04-29 19:22:38 +0300

    Add files via upload

---
+++ errno.h
@@ -0,0 +1,8 @@
+#if !defined(_ERRNO_H)
+#define _ERRNO_H
+
+extern int errno;
+
+#endif /* _ERRNO_H */
+
+error: expected unqualified-id before 'int'
---
+++ stdbool.h
@@ -0,0 +1,10 @@
+#include and b files are not relevant to this file. The content of stdbool.h is as follows:
+#if !defined(_STDBOOL_H)
+#define _STDBOOL_H
+
+typedef enum {
+    false = 0,
+    true = 1
+} bool;
+
+#endif /* _STDBOOL_H */
